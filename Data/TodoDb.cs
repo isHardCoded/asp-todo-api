7d113cf2e1e90cd9a251eb4c24bfa6351879f427
isHardCoded <ishardcoded@gmail.com>
2026-04-16 23:39:58 +0300

    feat: добавили in-memory базу данныхÐ, подключили документацию swagger

---
+++ Data/TodoDb.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using todo_api.Models;
+
+namespace todo_api.Data;
+
+public class TodoDb : DbContext
+{
+    public TodoDb(DbContextOptions<TodoDb> options) : base(options) {}
+    public DbSet<Todo> Todos => Set<Todo>();
+}
--- Program.cs
+++ Program.cs
@@ -1,8 +1,37 @@
+using Microsoft.EntityFrameworkCore;
+using todo_api.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
+builder.Services.AddDbContext<TodoDb>(options => options.UseInMemoryDatabase("TodoList"));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddOpenApiDocument(config =>
+{
+    config.DocumentName = "TodoAPI";
+    config.Title = "TodoAPI v1";
+    config.Version = "v1";
+});
 
 var app = builder.Build();
+if (app.Environment.IsDevelopment())
+{
+    app.UseOpenApi();
+    app.UseSwaggerUi(config =>
+    {
+        config.DocumentTitle = "TodoAPI";
+        config.Path = "/swagger";
+        config.DocumentPath = "/swagger/{documentName}/swagger.json";
+        config.DocExpansion = "list";
+    });
+}
+
+app.MapGet("/todo-items", async (TodoDb db) =>
+{
+    await db.Todos.ToListAsync();
+});
 
 if (app.Environment.IsDevelopment())
 {
@@ -10,7 +39,5 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
-app.MapGet("/", () => "Hello World!");
 
 app.Run();
